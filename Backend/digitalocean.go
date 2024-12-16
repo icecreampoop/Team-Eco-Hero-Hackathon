48d74eae1cf0898cb51e6a3970c3dad81a6eb6d5
IceCreamPoop <pchiku@hotmail.sg>
2024-12-16 13:06:37 +0800

    imports for digitalocean.go done

--- Backend/digitalocean.go
+++ Backend/digitalocean.go
@@ -1,2 +1,19 @@
 package main
 
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
+)
+
+struct (
+	type bucket {
+
+	}
+)
+
+func uploadToDO() {
+	Println("test")
+}
