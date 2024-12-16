ab281128c4ce9a1b6b665e925f869ae86100d60f
Edyth <jiayiho13@gmail.com>
2024-12-16 12:04:47 +0800

    base serverhandler.go logic

--- Backend/serverhandler.go
+++ Backend/serverhandler.go
@@ -1,2 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HandleHTTPIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Index")
+}
+
+func HandleHTTPItems(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Items")
+}
+
+func HandleHTTPUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "User")
+}
+
+func HandleHTTPBoard(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Leaderboard")
+}
+
+func HandleHTTPLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Login")
+}
+
+func main() {
+	// Create new HTTP mux
+	mux := http.NewServeMux()
+
+	// Register handlers
+	mux.HandleFunc("/", HandleHTTPIndex) // default handler
+	mux.HandleFunc("/index", HandleHTTPIndex)
+	mux.HandleFunc("/items", HandleHTTPItems)
+	mux.HandleFunc("/user", HandleHTTPUser)
+	mux.HandleFunc("/board", HandleHTTPBoard)
+	mux.HandleFunc("/login", HandleHTTPLogin)
+
+	// Start server
+	port := ":8080"
+	fmt.Printf("Server started at port %s\n", port)
+	if err := http.ListenAndServe(port, mux); err != nil {
+		log.Fatalf("Could not start server: %s\n", err)
+	}
+}
